40a9137448dbb05b07a19e5165a35dd75474ed68
RostSIT <streltsovrp@gmail.com>
2021-09-16 23:39:36 +0300

    Создайте класс Dog, конструктор __init__

--- 2.3 Практика Создание класса и его методов. Создайте класс Dog.py
+++ 2.3 Практика Создание класса и его методов. Создайте класс Dog.py
@@ -1,12 +1,14 @@
 
 '''Создайте класс Dog, у которого есть:
-конструктор __init__, принимающий 2 аргумента: name, age. 
+
+конструктор __init__, принимающий 2 аргумента: name, age.
 метод description, который возвращает строку в виде "<name> is <age> years old"
 метод speak принимающий один аргумент, который возвращает строку вида "<name> says <sound>";
 jack = Dog("Jack", 4)
+
 print(jack.description()) # распечатает 'Jack is 4 years old'
 print(jack.speak("Woof Woof")) # распечатает 'Jack says Woof Woof'
-print(jack.speak("Bow Wow")) # распечатает 'Jack says Bow Wow''''
+print(jack.speak("Bow Wow")) # распечатает 'Jack says Bow Wow'''
 
 class Dog:
     def __init__(self, name, age):

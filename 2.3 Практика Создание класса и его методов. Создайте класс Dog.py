25a6d5ffbd6fa95ba80b85fccec75a2e9eb722c7
RostSIT <streltsovrp@gmail.com>
2021-09-16 23:36:26 +0300

    конструктор __init__, принимающий 3 аргумента

--- 2.3 Практика Создание класса и его методов. Создайте класс Dog.py
+++ 2.3 Практика Создание класса и его методов. Создайте класс Dog.py
@@ -1,3 +1,15 @@
+
+'''Создайте класс Dog, у которого есть:
+
+конструктор __init__, принимающий 2 аргумента: name, age. 
+метод description, который возвращает строку в виде "<name> is <age> years old"
+метод speak принимающий один аргумент, который возвращает строку вида "<name> says <sound>";
+jack = Dog("Jack", 4)
+
+print(jack.description()) # распечатает 'Jack is 4 years old'
+print(jack.speak("Woof Woof")) # распечатает 'Jack says Woof Woof'
+print(jack.speak("Bow Wow")) # распечатает 'Jack says Bow Wow''''
+
 class Dog:
     def __init__(self, name, age):
         self.name = name
